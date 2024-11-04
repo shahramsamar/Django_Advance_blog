41b78b759633b8f5f042d5635f6c74235a59663a
shahramsamar <shahramsamar2010@gmail.com>
2024-11-04 12:35:31 +0330

    Update serializers.py
    added two way for category

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -7,11 +7,20 @@ from ...models import Post, Category
 #     id = serializers.IntegerField()
 #     title = serializers.CharField(max_length=255)
     
+class  CategorySerializer(serializers.ModelSerializer):
  
+    class Meta:
+        model = Category
+        fields =['id','name']
+
+    
 class  PostSerializer(serializers.ModelSerializer):
     snippet = serializers.ReadOnlyField(source="get_snippet")
     relative_url = serializers.URLField(source="get_absolute_api_url",read_only=True)
     absolute_url = serializers.SerializerMethodField()
+    # category = serializers.SlugRelatedField(many=False, slug_field="name",queryset=Category.objects.all())
+    # category = CategorySerializer()
+    
     
     class Meta:
         model = Post
@@ -23,14 +32,11 @@ class  PostSerializer(serializers.ModelSerializer):
         request = self.context.get("request")
         return request.build_absolute_uri(obj.pk)
     
+    def to_representation(self, instance):
+        return super().to_representation(instance)
     
     
     
-class  CategorySerializer(serializers.ModelSerializer):
- 
-    class Meta:
-        model = Category
-        fields =['id','name']
+    
 
-    
        
