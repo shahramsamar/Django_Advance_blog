3a9a5636f965ea20c2b583e5c913b2eb61d281ab
shahramsamar <shahramsamar2010@gmail.com>
2024-11-27 20:06:14 +0330

    Update serializers.py
    comment for each def

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -1,5 +1,6 @@
 from  rest_framework import serializers
 from ...models import Post, Category
+from accounts.models import Profile
 
 
 
@@ -18,15 +19,18 @@ class  PostSerializer(serializers.ModelSerializer):
     snippet = serializers.ReadOnlyField(source="get_snippet")
     relative_url = serializers.URLField(source="get_absolute_api_url", read_only=True)
     absolute_url = serializers.SerializerMethodField()
-    category = serializers.SlugRelatedField(many=False, slug_field="name", queryset=Category.objects.all())
+    category = serializers.SlugRelatedField(many=False,
+                                            slug_field="name", queryset=Category.objects.all())
     # category = CategorySerializer()
     
     
     class Meta:
         model = Post
         # fields = "__all__"
-        fields = ['id','author','title','category','content','snippet',
+        fields = ['id','title','image','category','content','snippet',
                  'relative_url','absolute_url','status','created_date','published_date']
+        # read_only_fields = ['author', 'created_date', 'updated_date']
+
         read_only_fields = ['author']
     
     def get_absolute_url(self, obj):
@@ -34,6 +38,9 @@ class  PostSerializer(serializers.ModelSerializer):
         return request.build_absolute_uri(obj.pk)
     #when we need to change data for show #
     def to_representation(self, instance):
+        """
+        Customize the data representation based on whether it's a single post or a list view.
+        """
         request = self.context.get("request")
         rep =  super().to_representation(instance)
         
@@ -41,17 +48,39 @@ class  PostSerializer(serializers.ModelSerializer):
         # if request.parser_context.get("kwargs").get('pk'):
         #     rep['state'] = 'single'
         if request.parser_context.get("kwargs").get('pk'):
+            # Single post view
             rep.pop('snippet')
             rep.pop('relative_url')
             rep.pop('absolute_url')
         else:
+            # List view
             rep.pop('content', None)
-
-        rep['category'] = CategorySerializer(instance.category).data
+            
+        # Add detailed category data
+        rep['category'] = CategorySerializer(instance.category, context={'request':request}).data
         return rep
     
-    
-    
-    
+        # def create(self, validated_data):
+        #     """
+        #     Automatically set the author field to the currently authenticated user's profile.
+        #     """
+        #     # validated_data['author '] = Profile.objects.get(user = self.context.get('request').user.id)
+        #     # validated_data['author'] = Profile.objects.get(user__id = self.context.get('request').user.id)
+        #     # return super().create(validated_data)
+            
+        #     user = self.context['request'].user
+        #     profile = Profile.objects.get(user=user)
+        #     validated_data['author'] = profile  # Corrected key
+        #     return super().create(validated_data)
+            
+        def create(self, validated_data):
+            user = self.context['request'].user
+            try:
+                profile = Profile.objects.get(user=user)  # Get the user's profile
+            except Profile.DoesNotExist:
+                raise serializers.ValidationError("The authenticated user does not have an associated profile.")
+            
+            validated_data['author'] = profile  # Assign the profile as the author
+            return super().create(validated_data)
 
-       
+        
